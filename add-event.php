c8f85ef2a8eac0878d18f7d52de3bcdd4a98546e
Quinten O'Neal <qtylero@gmail.com>
2018-04-29 22:03:06 -0400

    default photo if thumbnail doesnt exist

--- add-event.php
+++ add-event.php
@@ -5,6 +5,9 @@
 	$artist = $_POST['artist'];
 	$venue = $_POST['venue'];
 	$url = $_POST['url'];
+		if (empty($url)) {
+			$url = "./images/default.jpg";
+		}
 	$date = $_POST['date'];
 	$username = $_SESSION['user']; 
 	// echo $artist;
@@ -15,7 +18,7 @@
 
 	
 	
-	$dupesql = "SELECT * FROM events where (username = '$username' AND artist = '$artist' AND thumbail = '$url' AND date = '$date')";
+	$dupesql = "SELECT * FROM events where (username = '$username' AND artist = '$artist' AND thumbnail = '$url' AND date = '$date')";
 
 	$duperaw = mysqli_query($conn, $dupesql);
 	if (!$duperaw) die($conn->error);
@@ -25,7 +28,7 @@
 		echo $duplicate;
 
 	} else {
-		$sql = "INSERT INTO events (username, artist, thumbail, date, venue) VALUES ('$username', '$artist', '$url', '$date', '$venue')";
+		$sql = "INSERT INTO events (username, artist, thumbnail, date, venue) VALUES ('$username', '$artist', '$url', '$date', '$venue')";
 		mysqli_query ($conn, $sql);
 		$Added = "Added Event";
 		echo $Added;
